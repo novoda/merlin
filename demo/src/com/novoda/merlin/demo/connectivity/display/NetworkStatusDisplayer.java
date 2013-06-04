f17a0b569bb1b6c7ad60d0dc92fe8d9815ccb5a7
Adam <adam@novoda.com>
2013-06-04 20:07:23 +0100

    resolving directory mishmash

--- demo/src/com/novoda/merlin/demo/connectivity/display/NetworkStatusDisplayer.java
+++
@@ -1,8 +0,0 @@
-package com.novoda.merlin.demo.connectivity.display;
-
-public interface NetworkStatusDisplayer {
-    void displayConnected();
-    void displayDisconnected();
-    void reset();
-}
-
