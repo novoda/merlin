984f16a5e1fdf77af7601effa7a9da1f93da18fe
Ryan Feline <ryan.feline@novoda.com>
2014-09-09 09:27:18 +0100

    Adds unit test to verify that the onDisconnect callback is called for a singular disconnectable.

--- unit-tests/src/test/java/com/novoda/merlin/registerable/disconnection/DisconnectorShould.java
+++ unit-tests/src/test/java/com/novoda/merlin/registerable/disconnection/DisconnectorShould.java
@@ -1,7 +1,35 @@
 package com.novoda.merlin.registerable.disconnection;
 
-import static org.junit.Assert.*;
+import com.novoda.merlin.registerable.MerlinConnector;
+import com.novoda.merlin.registerable.MerlinRegisterer;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class DisconnectorShould {
 
+    private MerlinConnector<Disconnectable> merlinConnector;
+
+    private Disconnector disconnector;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+        merlinConnector = new MerlinRegisterer<Disconnectable>();
+        disconnector = new Disconnector(merlinConnector);
+    }
+
+    @Test
+    public void callback_registered_disconnectables() throws Exception {
+        Disconnectable disconnectable = mock(Disconnectable.class);
+        merlinConnector.register(disconnectable);
+
+        disconnector.onDisconnect();
+
+        verify(disconnectable).onDisconnect();
+    }
 }
