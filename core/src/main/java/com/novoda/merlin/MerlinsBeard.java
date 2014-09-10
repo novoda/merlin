9c8fc1cf55705b557453e5ebcd44e6d1ececc2e6
Ryan Feline <ryan.feline@novoda.com>
2014-09-10 17:46:12 +0100

    Adds javadoc comments to MerlinsBeard.

--- core/src/main/java/com/novoda/merlin/MerlinsBeard.java
+++ core/src/main/java/com/novoda/merlin/MerlinsBeard.java
@@ -4,10 +4,19 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+/**
+ * This class provides a mechanism for retrieving the current
+ * state of a network connection given an application context.
+ */
 public class MerlinsBeard {
 
     private ConnectivityManager connectivityManager;
 
+    /**
+     *
+     * @param context used to retrieve a Connectivity Manager.
+     * @return MerlinsBeard
+     */
     public static MerlinsBeard from(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         return new MerlinsBeard(connectivityManager);
@@ -17,6 +26,11 @@ public class MerlinsBeard {
         this.connectivityManager = connectivityManager;
     }
 
+    /**
+     * Provides a boolean representing whether a network connection is available.
+     * NOTE: This does not ping a host in order to verify the network connection.
+     * @return boolean
+     */
     public boolean isConnected() {
         NetworkInfo activeNetworkInfo = getNetworkInfo();
         return (activeNetworkInfo != null && activeNetworkInfo.isConnected());
