f3d16ca6fde796a648b300d5d197f3cd2b6ae863
Daniele Conti <daniele.conti@novoda.com>
2016-04-15 14:14:59 +0100

    add empty line before constructor

--- core/src/main/java/com/novoda/merlin/service/request/RequestException.java
+++ core/src/main/java/com/novoda/merlin/service/request/RequestException.java
@@ -3,6 +3,7 @@ package com.novoda.merlin.service.request;
 import java.io.IOException;
 
 public class RequestException extends RuntimeException {
+
     public RequestException(Throwable e) {
         super(e);
     }
