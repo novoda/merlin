6bc6189e138a29236f240c740aeb43e68d268d25
Joe Timmins <joe@novoda.com>
2016-06-10 15:55:51 +0100

    Import inner HostPinger classes directly

--- core/src/main/java/com/novoda/merlin/service/PingTaskFactory.java
+++ core/src/main/java/com/novoda/merlin/service/PingTaskFactory.java
@@ -1,12 +1,15 @@
 package com.novoda.merlin.service;
 
+import static com.novoda.merlin.service.HostPinger.PingerCallback;
+import static com.novoda.merlin.service.HostPinger.ResponseCodeFetcher;
+
 class PingTaskFactory {
 
-    private final HostPinger.PingerCallback pingerCallback;
-    private final HostPinger.ResponseCodeFetcher responseCodeFetcher;
+    private final PingerCallback pingerCallback;
+    private final ResponseCodeFetcher responseCodeFetcher;
     private final ResponseCodeValidator responseCodeValidator;
 
-    PingTaskFactory(HostPinger.PingerCallback pingerCallback, HostPinger.ResponseCodeFetcher responseCodeFetcher, ResponseCodeValidator responseCodeValidator) {
+    PingTaskFactory(PingerCallback pingerCallback, ResponseCodeFetcher responseCodeFetcher, ResponseCodeValidator responseCodeValidator) {
         this.pingerCallback = pingerCallback;
         this.responseCodeFetcher = responseCodeFetcher;
         this.responseCodeValidator = responseCodeValidator;
