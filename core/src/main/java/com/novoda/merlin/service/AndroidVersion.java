6b107263252e75bb26a72b89482bc4cc59e27d68
Ryan Feline <ryan.feline@novoda.com>
2017-04-13 13:04:15 +0100

    Create utility class for checking the current build against a given AndroidVersion.

---
+++ core/src/main/java/com/novoda/merlin/service/AndroidVersion.java
@@ -0,0 +1,21 @@
+package com.novoda.merlin.service;
+
+import android.os.Build;
+
+public class AndroidVersion {
+
+    private final int deviceVersion;
+
+    public AndroidVersion() {
+        this(Build.VERSION.SDK_INT);
+    }
+
+    private AndroidVersion(int deviceVersion) {
+        this.deviceVersion = deviceVersion;
+    }
+
+    public boolean isMarshmallowOrHigher() {
+        return deviceVersion >= Build.VERSION_CODES.M;
+    }
+
+}
