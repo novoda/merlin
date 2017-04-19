aeb46afc60ca14d0dc577a5505b9571e809a332e
Ryan Feline <ryan.feline@novoda.com>
2017-04-19 15:44:42 +0100

    Add tests for MerlinNetworkCallbacks.

--- core/src/main/java/com/novoda/merlin/receiver/ConnectivityChangeEvent.java
+++ core/src/main/java/com/novoda/merlin/receiver/ConnectivityChangeEvent.java
@@ -29,4 +29,43 @@ public class ConnectivityChangeEvent {
     public NetworkStatus asNetworkStatus() {
         return isConnected() ? NetworkStatus.newAvailableInstance() : NetworkStatus.newUnavailableInstance();
     }
+
+    @Override
+    public String toString() {
+        return "ConnectivityChangeEvent{" +
+                "isConnected=" + isConnected +
+                ", info='" + info + '\'' +
+                ", reason='" + reason + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ConnectivityChangeEvent that = (ConnectivityChangeEvent) o;
+
+        if (isConnected != that.isConnected) {
+            return false;
+        }
+        if (info != null ? !info.equals(that.info) : that.info != null) {
+            return false;
+        }
+        return reason != null ? reason.equals(that.reason) : that.reason == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (isConnected ? 1 : 0);
+        result = 31 * result + (info != null ? info.hashCode() : 0);
+        result = 31 * result + (reason != null ? reason.hashCode() : 0);
+        return result;
+    }
+
 }
