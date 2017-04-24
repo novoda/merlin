69997c5f47b6878a4ed5622b6eebd18e8e7f50c6
Borja Quevedo <borjota@gmail.com>
2017-04-24 11:28:09 +0200

    Create RxJava wrapper for Merlin

---
+++ merlin-rxjava/src/main/java/com/novoda/merlin/MerlinObservable.java
@@ -0,0 +1,56 @@
+package com.novoda.merlin;
+
+import android.content.Context;
+
+import com.novoda.merlin.registerable.bind.Bindable;
+import com.novoda.merlin.registerable.connection.Connectable;
+import com.novoda.merlin.registerable.disconnection.Disconnectable;
+
+import rx.Emitter;
+import rx.Observable;
+import rx.functions.Action1;
+import rx.functions.Cancellable;
+
+public class MerlinObservable {
+
+    public static Observable<NetworkStatus.State> from(Context context) {
+        MerlinBuilder builder = new MerlinBuilder();
+        builder.withAllCallbacks();
+        final Merlin merlin = builder.build(context);
+
+        return Observable.create(new Action1<Emitter<NetworkStatus.State>>() {
+            @Override
+            public void call(final Emitter<NetworkStatus.State> emitter) {
+                merlin.registerConnectable(new Connectable() {
+                    @Override
+                    public void onConnect() {
+                        emitter.onNext(NetworkStatus.State.AVAILABLE);
+                    }
+                });
+                merlin.registerDisconnectable(new Disconnectable() {
+                    @Override
+                    public void onDisconnect() {
+                        emitter.onNext(NetworkStatus.State.UNAVAILABLE);
+                    }
+                });
+                merlin.registerBindable(new Bindable() {
+                    @Override
+                    public void onBind(NetworkStatus networkStatus) {
+                        NetworkStatus.State current = networkStatus.isAvailable() ? NetworkStatus.State.AVAILABLE : NetworkStatus.State.UNAVAILABLE;
+                        emitter.onNext(current);
+                    }
+                });
+
+                emitter.setCancellation(new Cancellable() {
+                    @Override
+                    public void cancel() throws
+                                         Exception {
+                        merlin.unbind();
+                    }
+                });
+
+                merlin.bind();
+            }
+        }, Emitter.BackpressureMode.BUFFER);
+    }
+}
