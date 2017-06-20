b9f60b5c8266772e199250e63c1223b84f569633
Ryan Feline <ryan.feline@novoda.com>
2017-06-20 22:29:54 +0100

    Set snackbar to top of parent.

--- demo/src/main/java/com/novoda/merlin/demo/connectivity/display/MerlinSnackbar.java
+++ demo/src/main/java/com/novoda/merlin/demo/connectivity/display/MerlinSnackbar.java
@@ -3,7 +3,9 @@ package com.novoda.merlin.demo.connectivity.display;
 import android.content.res.Resources;
 import android.support.annotation.StringRes;
 import android.support.design.widget.Snackbar;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.novoda.merlin.demo.R;
 
@@ -14,16 +16,14 @@ class MerlinSnackbar {
     static MerlinSnackbar withDuration(Resources resources, View attachTo, @StringRes int message) {
         int duration = resources.getInteger(R.integer.snackbar_duration);
         Snackbar snackbar = Snackbar.make(attachTo, message, duration);
+        View view = snackbar.getView();
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) view.getLayoutParams();
+        layoutParams.gravity = Gravity.TOP;
         return new MerlinSnackbar(snackbar);
     }
 
     private MerlinSnackbar(Snackbar snackbar) {
         this.snackbar = snackbar;
-    }
-
-    MerlinSnackbar setAction(@StringRes int textResourceId, View.OnClickListener listener) {
-        snackbar.setAction(textResourceId, listener);
-        return this;
     }
 
     void show() {
