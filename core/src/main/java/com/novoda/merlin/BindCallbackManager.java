6e4ee10d48792af1620ee52726c458634b3b76d5
Ryan Feline <ryan.feline@novoda.com>
2018-02-13 17:03:34 +0000

    Make BindCallbackManager package protected. This is only used internally.

--- core/src/main/java/com/novoda/merlin/BindCallbackManager.java
+++ core/src/main/java/com/novoda/merlin/BindCallbackManager.java
@@ -1,12 +1,12 @@
 package com.novoda.merlin;
 
-public class BindCallbackManager extends MerlinCallbackManager<Bindable> {
+class BindCallbackManager extends MerlinCallbackManager<Bindable> {
 
-    public BindCallbackManager(Register<Bindable> register) {
+    BindCallbackManager(Register<Bindable> register) {
         super(register);
     }
 
-    public void onMerlinBind(NetworkStatus networkStatus) {
+    void onMerlinBind(NetworkStatus networkStatus) {
         Logger.d("onBind");
         for (Bindable bindable : registerables()) {
             bindable.onBind(networkStatus);
