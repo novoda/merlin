2ddb1df70b7d2d9f19fc58f0912200da1d893151
Ryan Feline <ryan.feline@novoda.com>
2018-02-13 17:05:46 +0000

    Make ConnectCallbackManager package protected. This is only used internally.

--- core/src/main/java/com/novoda/merlin/ConnectCallbackManager.java
+++ core/src/main/java/com/novoda/merlin/ConnectCallbackManager.java
@@ -1,12 +1,12 @@
 package com.novoda.merlin;
 
-public class ConnectCallbackManager extends MerlinCallbackManager<Connectable> {
+class ConnectCallbackManager extends MerlinCallbackManager<Connectable> {
 
-    public ConnectCallbackManager(Register<Connectable> register) {
+    ConnectCallbackManager(Register<Connectable> register) {
         super(register);
     }
 
-    public void onConnect() {
+    void onConnect() {
         Logger.d("onConnect");
         for (Connectable connectable : registerables()) {
             connectable.onConnect();
