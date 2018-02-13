4539e871c756ffe2a22c12d45632680839e030dc
Ryan Feline <ryan.feline@novoda.com>
2018-02-13 17:01:38 +0000

    Fix imports on Bindable.

--- core/src/main/java/com/novoda/merlin/Bindable.java
+++ core/src/main/java/com/novoda/merlin/Bindable.java
@@ -1,8 +1,5 @@
 package com.novoda.merlin;
 
-import com.novoda.merlin.NetworkStatus;
-import com.novoda.merlin.Registerable;
-
 public interface Bindable extends Registerable {
     void onBind(NetworkStatus networkStatus);
 }
