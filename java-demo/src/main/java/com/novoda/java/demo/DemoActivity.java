a4fc395996b9ec114b1ebef065168c417e294caf
Ryan Feline <ryan.feline@novoda.com>
2018-12-19 16:29:56 +0000

    Remove unnecessary resources from java demo and hook up to demo resources module.

---
+++ java-demo/src/main/java/com/novoda/java/demo/DemoActivity.java
@@ -0,0 +1,12 @@
+package com.novoda.java.demo;
+
+import android.os.Bundle;
+import com.novoda.demo.resources.CommonDemoActivity;
+
+public class DemoActivity extends CommonDemoActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
