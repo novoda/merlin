8664b1ff6401b037944511c6313314883df5ef6a
Ryan Feline <ryan.feline@novoda.com>
2018-12-19 15:14:15 +0000

    Reorganise package structure for java demo.

--- java-demo/src/main/java/com/novoda/java/demo/DemoActivity.java
+++ java-demo/src/main/java/com/novoda/java/demo/DemoActivity.java
@@ -1,7 +1,7 @@
-package com.novoda.demo;
+package com.novoda.java.demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class DemoActivity extends AppCompatActivity {
 
