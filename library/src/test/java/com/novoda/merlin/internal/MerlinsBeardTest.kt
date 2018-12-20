f6aa16f3ed9ce6094bc8ab35f4b727df1ef13720
Ryan Feline <ryan.feline@novoda.com>
2018-12-20 16:42:00 +0000

    Add tests for mobile subtype.

--- library/src/test/java/com/novoda/merlin/internal/MerlinsBeardTest.kt
+++ library/src/test/java/com/novoda/merlin/internal/MerlinsBeardTest.kt
@@ -29,6 +29,15 @@ object MerlinsBeardTest : Spek({
             }
         }
 
+        on("checking mobile subtype") {
+            val merlinsBeard = MerlinsBeard(connectivityManager)
+            val mobileNetworkSubtype = merlinsBeard.mobileNetworkSubtype()
+
+            it("it is empty") {
+                assertThat(mobileNetworkSubtype).isEmpty()
+            }
+        }
+
         and("android version is Lollipop or higher") {
             val merlinsBeard = MerlinsBeard(connectivityManager, AndroidVersion(Build.VERSION_CODES.LOLLIPOP))
 
@@ -135,6 +144,7 @@ object MerlinsBeardTest : Spek({
         }
         val networkInfo: NetworkInfo = mock {
             on { isConnected }.thenReturn(true)
+            on { subtypeName }.thenReturn("subtype")
         }
         val connectivityManager: ConnectivityManager = mock {
             on { activeNetworkInfo }.thenReturn(networkInfo)
@@ -142,6 +152,15 @@ object MerlinsBeardTest : Spek({
             on { getNetworkCapabilities(network) }.thenReturn(networkCapabilities)
             on { getNetworkInfo(network) }.thenReturn(networkInfo)
             on { getNetworkInfo(ConnectivityManager.TYPE_MOBILE) }.thenReturn(networkInfo)
+        }
+
+        on("checking mobile subtype") {
+            val merlinsBeard = MerlinsBeard(connectivityManager)
+            val mobileNetworkSubtype = merlinsBeard.mobileNetworkSubtype()
+
+            it("is is subtype") {
+                assertThat(mobileNetworkSubtype).isEqualTo("subtype")
+            }
         }
 
         and("android version is lower than Lollipop") {
